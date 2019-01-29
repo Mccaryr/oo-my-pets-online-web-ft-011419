d6aba652b99b7ec8c73b89225caa201d446fdf88
Robert McCary <robert.mccary@yahoo.com>
2019-01-29 17:48:12 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,31 @@
 class Owner
-  # code goes here
+  @@count = 0
+  @@all = []
+  attr_reader :species
+  attr_accessor :name, :pets
+  def initialize(species)
+    @species=species
+    @name=name
+    @pets = {fishes: [], cats: [], dogs: []}
+    @@all << self
+    @@count += 1
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+    @@count = 0
+  end
+
+  def self.count
+    @@count
+  end
+
+  def say_species
+     "I am a #{self.species}."
+  end
+
 end
